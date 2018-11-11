3f0482f675331e61bf6c09691b1478c814dfbb1d
ezksd <yangli245@elive.com>
2018-11-11 15:17:37 +0800

    ...

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,64 +1,63 @@
 {-# LANGUAGE LambdaCase #-}
-module Interpreter (repl) where
-import           System.IO
-import           Data.IORef
+{-# LANGUAGE MultiWayIf #-}
+module Interpreter
+    ( repl
+    )
+where
 import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.Ref
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.Reader
+import           Paths_scheme
+import           Data.IORef
 import qualified Data.Map.Strict               as Map
 import           Parser
-import           Prelude                 hiding ( lookup,init )
+import           Prelude                 hiding ( init
+                                                , lookup
+                                                )
 import           Prims
 import           Scheme
+import           System.IO
+import           System.IO.Error
 
 type Interpreter a = ReaderT Env (ExceptT ScmErr IO) a
 
 repl :: IO ()
-repl = init >>= loop' ""
-
-init :: IO Env
-init = do
-    content <- readFile "primit.scm"
+repl = do
+    content <- getDataFileName "primit.scm" >>= readFile
     ref     <- newIORef [Map.fromList primitives]
     eval content ref
-    pure ref
+    putStrLn "press Ctrl+D to quit."
+    catchIOError (loop "" ref)
+                 (\e -> if isEOFError e then putStrLn "" else ioError e)
 
-loop :: Env -> IO ()
-loop env = do
-    putStr "> "
-    hFlush stdout
-    content <- getLine
-    eval content env
-    loop env
-
-loop' :: String -> Env -> IO()
-loop' s env = do
+loop :: String -> Env -> IO ()
+loop s env = do
     putStr output
     hFlush stdout
     content <- getLine
-    let s' = s ++ content 
-        n = count s' 
-        in case () of
-            _ | n >  0 -> loop' s' env
-              | n == 0 -> eval s' env >> loop' "" env
-              | n <  0 -> putStrLn "illgal input" >> loop' "" env
-    where 
-        count = count' 0
-        count' n [] = n
-        count' n ('(':xs) = count' (n + 1) xs
-        count' n (')':xs) = count' (n - 1) xs
-        count' n (_:xs)   = count' n       xs
-        num = count s
-        output = case count s of
-            n | n == 0 -> "> "
-              | n > 0 -> space (n*2)
-              | n < 0 -> "illegal input"
-        space n = space' n []  
-        space' 0 xs = xs
-        space' n xs = space' (n - 1) ("  " ++ xs)
+    let s' = s ++ content
+        n  = count s'
+    if
+        | n > 0  -> loop s' env
+        | n == 0 -> eval s' env >> loop "" env
+        | True   -> putStrLn "illgal input" >> loop "" env
+  where
+    count = count' 0
+    count' n []         = n
+    count' n ('(' : xs) = count' (n + 1) xs
+    count' n (')' : xs) = count' (n - 1) xs
+    count' n (_   : xs) = count' n xs
+    num    = count s
+    output = case count s of
+        n | n == 0 -> "> "
+          | n > 0  -> space (n * 2)
+          | n < 0  -> "illegal input"
+    space n = space' n []
+    space' 0 xs = xs
+    space' n xs = space' (n - 1) ("  " ++ xs)
 
 eval :: String -> Env -> IO ()
 eval s env = case parseAll s of
@@ -72,8 +71,16 @@ eval s env = case parseAll s of
     m Void = mempty
     m x    = show x ++ "\n"
 
-define :: String -> Expr -> Interpreter Expr
-define k v = ask >>= (`modifyRef` (\(x : xs) -> Map.insert k v x : xs)) >> pure Void
+define :: String -> Expr -> Interpreter ()
+define k v = ask >>= (`modifyRef` (\(x : xs) -> Map.insert k v x : xs))
+
+update :: String -> Expr -> Interpreter ()
+update k v = join $ writeRef <$> ask <*> (ask >>= readRef >>= update' [])
+  where
+    update' _   []       = lift (throwE ("unbounded identifer :" ++ k))
+    update' pre (x : xs) = if Map.member k x
+        then pure (reverse pre ++ [Map.insert k v x] ++ xs)
+        else update' (x : pre) xs
 
 lookup :: String -> Interpreter Expr
 lookup k = ask >>= readRef >>= maybe err pure . lookup'
@@ -81,8 +88,8 @@ lookup k = ask >>= readRef >>= maybe err pure . lookup'
     err     = lift (throwE ("unbounded identifer :" ++ k))
     lookup' = foldr ((<|>) . Map.lookup k) empty
 
-push :: [(String,Expr)] -> Interpreter ()
-push xs = ask >>= (`modifyRef` (Map.fromList xs:))
+push :: [(String, Expr)] -> Interpreter ()
+push xs = ask >>= (`modifyRef` (Map.fromList xs :))
 
 pop :: Interpreter ()
 pop = ask >>= (`modifyRef` tail)
@@ -90,31 +97,37 @@ pop = ask >>= (`modifyRef` tail)
 interpret :: Expr -> Interpreter Expr
 interpret (Symbol x) = lookup x
 interpret (List   x) = case x of
-    Symbol "define" : List (Symbol f : xs) : body -> ask >>= define f . Closure (unSymbols xs) body
+    Symbol "define" : List (Symbol f : xs) : body ->
+        ask >>= define f . Closure (unSymbols xs) body >> pure Void
     Symbol "lambda" : List xs : body -> Closure (unSymbols xs) body <$> ask
-    Symbol "let" : List binding : body -> (extract binding >>= push) *> interpretBody body <* pop
-    [Symbol "set!"  , Symbol k, v] -> interpret v >>= define k
-    [Symbol "define", Symbol k, v] -> interpret v >>= define k
+    Symbol "let" : List binding : body ->
+        (extract binding >>= push) *> interpretBody body <* pop
+    [Symbol "set!"  , Symbol k, v] -> interpret v >>= update k >> pure Void
+    [Symbol "define", Symbol k, v] -> interpret v >>= define k >> pure Void
     [Symbol "quote", e]            -> pure e
     [Symbol "if", p, v1, v2]       -> do
         (Bool b) <- interpret p
         interpret (if b then v1 else v2)
-    xs -> do
+    xs                             -> do
         (h : params) <- interpretAll xs
         case h of
-            (Closure ps body env') -> push (zip ps params) *> interpretBody body <* pop
+            (Closure ps body env') ->
+                push (zip ps params) *> interpretBody body <* pop
             (Func func) -> lift (func params)
             _           -> lift (throwE "not a function")
   where
     interpretBody = (last <$>) . interpretAll
-    unSymbols = ((\case (Symbol input) -> input) <$>)
+    unSymbols =
+        ((\case
+             (Symbol input) -> input
+         ) <$>
+        )
     extract []                        = pure []
     extract (List [Symbol s, v] : xs) = do
         e  <- interpret v
         es <- extract xs
         pure ((s, e) : es)
 interpret x = pure x
-
 
 interpretAll :: [Expr] -> Interpreter [Expr]
 interpretAll = sequence . (interpret <$>)
--- src/Prims.hs
+++ src/Prims.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
-module Prims (primitives)
+module Prims
+    ( primitives
+    )
 where
+import           Control.Monad
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Except
 import           Scheme
@@ -14,14 +17,18 @@ numericOp op = \case
     _                    -> throwE "paramters not match"
 
 unaryOp :: (Expr -> Either ScmErr Expr) -> IFunc
-unaryOp op = wrap (\case
+unaryOp op = wrap
+    (\case
         [x] -> op x
-        _   -> Left "paramters not match")
+        _   -> Left "paramters not match"
+    )
 
 binaryOp :: (Expr -> Expr -> Either ScmErr Expr) -> IFunc
-binaryOp op = wrap (\case
+binaryOp op = wrap
+    (\case
         [a, b] -> op a b
-        _      -> Left "paramters not match")
+        _      -> Left "paramters not match"
+    )
 
 and' :: [Expr] -> Either ScmErr Expr
 and' []            = pure (Bool True)
@@ -34,45 +41,88 @@ or' (Bool b : xs) = if b then pure (Bool True) else or' xs
 or' _             = Left "bot boolean type"
 
 primitives :: [(String, Expr)]
-primitives = (\(a, b) -> (a, Func b)) <$> 
-    [ ("+", caculate (+))
-    , ("-", caculate (-))
-    , ("*", caculate (*))
-    , ("/", caculate div)
-    , (">", comp (>))
-    , ("=", comp (==))
-    , ("<", comp (<))
-    , ( "list?", unaryOp (\case
-              List _ -> pure (Bool True)
-              _      -> pure (Bool False)))
-    , ( "pair?", unaryOp (\case
-              Pair _ _ -> pure (Bool True)
-              _ -> pure (Bool False)))
-    , ( "null?", unaryOp (\case
-              List [] -> pure (Bool True)
-              _       -> pure (Bool False)))
-    , ( "car", unaryOp (\case
-              List (a : _) -> pure a
-              Pair a _     -> pure a
-              _            -> Left "not a pair/list"))
-    , ( "cdr", unaryOp (\case
-              List (_ : b) -> pure (List b)
-              Pair _ b     -> pure b
-              _            -> Left "not a pair/list"))
-    , ( "cons", binaryOp (\a b -> case b of
-              List bs -> pure (List (a : bs))
-              _       -> pure (Pair a b)))
-    , ("and", wrap and')
-    , ("or" , wrap or')
-    , ( "not", unaryOp (\case
-              Bool b -> pure (Bool (not b))
-              _      -> Left "not boolean type"))
-    , ( "display", \case
-          [x] -> lift (print x) >> pure (List [])
-          _   -> throwE "parameters not match")
-    , ( "newline", \case
-          [] -> lift (putStr "\n") >> pure (List [])
-          _  -> throwE "parameters not match")]
+primitives =
+    (\(a, b) -> (a, Func b))
+        <$> [ ("+", caculate (+))
+            , ("-", caculate (-))
+            , ("*", caculate (*))
+            , ("/", caculate div)
+            , (">", comp (>))
+            , ("=", comp (==))
+            , ("<", comp (<))
+            , ( "list?"
+              , unaryOp
+                  (\case
+                      List _ -> pure (Bool True)
+                      _      -> pure (Bool False)
+                  )
+              )
+            , ( "pair?"
+              , unaryOp
+                  (\case
+                      Pair _ _ -> pure (Bool True)
+                      _        -> pure (Bool False)
+                  )
+              )
+            , ( "null?"
+              , unaryOp
+                  (\case
+                      List [] -> pure (Bool True)
+                      _       -> pure (Bool False)
+                  )
+              )
+            , ( "car"
+              , unaryOp
+                  (\case
+                      List (a : _) -> pure a
+                      Pair a _     -> pure a
+                      _            -> Left "not a pair/list"
+                  )
+              )
+            , ( "cdr"
+              , unaryOp
+                  (\case
+                      List (_ : b) -> pure (List b)
+                      Pair _ b     -> pure b
+                      _            -> Left "not a pair/list"
+                  )
+              )
+            , ( "cons"
+              , binaryOp
+                  (\a b -> case b of
+                      List bs -> pure (List (a : bs))
+                      _       -> pure (Pair a b)
+                  )
+              )
+            , ( "foldr"
+              , \case
+                  [Func f, x, List xs] -> foldM (\a b -> f [a, b]) x xs
+                  _                    -> throwE "parameters not match"
+              )
+            , ( "reverse"
+              , \case
+                  [List xs] -> pure (List (reverse xs))
+              )
+            , ("and", wrap and')
+            , ("or" , wrap or')
+            , ( "not"
+              , unaryOp
+                  (\case
+                      Bool b -> pure (Bool (not b))
+                      _      -> Left "not boolean type"
+                  )
+              )
+            , ( "display"
+              , \case
+                  [x] -> lift (print x) >> pure (List [])
+                  _   -> throwE "parameters not match"
+              )
+            , ( "newline"
+              , \case
+                  [] -> lift (putStr "\n") >> pure (List [])
+                  _  -> throwE "parameters not match"
+              )
+            ]
   where
     caculate op = numericOp (\a b -> Number (op a b))
     comp op = numericOp (\a b -> Bool (op a b))
